0380ff2ab89bf57962e4acddd39cf9eb925c26d0
BASH <ahamedb1991@gmail.com>
2019-12-22 04:46:37 -0500

    update

--- src/Containers/About.js
+++ src/Containers/About.js
@@ -4,7 +4,7 @@ class About extends React.Component {
     render() {
         return (
 
-            <div>
+            <div className="">
                 Hello
             </div>
         );
--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -12,7 +12,7 @@ class Home extends React.Component {
 
     render() {
         return (
-            <div >
+            <div className="center">
                 <div className="display-4 m-5">
                     Welcome!
                     <div className="h4">
