ed0d031fd795e5031881f020adefb5a27f8ee88c
Bash <ahamedb1991@gmail.com>
2021-02-07 16:26:45 -0500

    Updates

---
+++ precache-manifest.111f79e443f10427da8679da26657e2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fd9e7f3bcd6d88969f7ba65cab56e67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb6e8530287cc32ae970",
+    "url": "/static/css/main.63bb63e2.chunk.css"
+  },
+  {
+    "revision": "285c3337c88c8bb53e1b",
+    "url": "/static/js/2.01db38b1.chunk.js"
+  },
+  {
+    "revision": "95fcf2f101cb3987411cb7a978b5c0b1",
+    "url": "/static/js/2.01db38b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb6e8530287cc32ae970",
+    "url": "/static/js/main.ddb4d88f.chunk.js"
+  },
+  {
+    "revision": "25dea7817254524c0809",
+    "url": "/static/js/runtime-main.cadba644.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ahamedbashir/CabVisualization/precache-manifest.fa1fccc6cef287e125520108a157f8c6.js"
+  "/precache-manifest.111f79e443f10427da8679da26657e2a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ahamedbashir/CabVisualization/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
