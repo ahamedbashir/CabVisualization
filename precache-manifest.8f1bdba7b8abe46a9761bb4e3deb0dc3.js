700fea207a755f8c4e86b3fdf561ff2c5fe402fd
Bash <ahamedb1991@gmail.com>
2021-02-07 16:42:35 -0500

    Updates

--- precache-manifest.8f1bdba7b8abe46a9761bb4e3deb0dc3.js
+++ precache-manifest.8f1bdba7b8abe46a9761bb4e3deb0dc3.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dbfcb8c84ca145df6bdae1b821b17062",
+    "revision": "1aa89e4f28c3df776ff2c5e5ef358f2e",
     "url": "/index.html"
   },
   {
-    "revision": "01d6710ebe7ca30b7f71",
+    "revision": "bb6e8530287cc32ae970",
     "url": "/static/css/main.63bb63e2.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.01db38b1.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "01d6710ebe7ca30b7f71",
-    "url": "/static/js/main.de0165da.chunk.js"
+    "revision": "bb6e8530287cc32ae970",
+    "url": "/static/js/main.ddb4d88f.chunk.js"
   },
   {
     "revision": "25dea7817254524c0809",
